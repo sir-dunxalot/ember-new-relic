7a5c870c05019f56beb48f1bf4c08f23568e6d87
Garrick <gcheung@fitbit.com>
2016-10-13 10:30:10 -0700

    Test writeTrackingCodeTree actually writes a file and the contents are as expected. Exclude node_tests/tmp dir that broccoli writes to in the test.

--- node_tests/index-test.js
+++ node_tests/index-test.js
@@ -1,6 +1,9 @@
 /* globals QUnit, test, EmberNewRelic */
 
 var objectAssign = require('object-assign');
+var broccoli = require('broccoli');
+var fs = require('fs');
+var path = require('path');
 
 QUnit.module("ember-new-relic | When config['ember-new-relic'].spaMonitoring is false", {
   setup: function() {
@@ -60,4 +63,34 @@ test('getNewRelicTrackingCode returns spaTrackingCode', function(assert) {
   assert.equal(
     EmberNewRelic.getNewRelicTrackingCode(this.newRelicConfig),
     EmberNewRelic.spaTrackingCode(this.newRelicConfig));
+});
+
+QUnit.module("ember-new-relic | When outputPath, applicationId, and licenseKey are defined", {
+  setup: function() {
+    this.newRelicConfig = EmberNewRelic.getNewRelicConfig({
+        spaMonitoring: true,
+        applicationId: 'test application ID',
+        licenseKey: 'test license key',
+        agent: 'js-agent.newrelic.com/nr-spa-963.min.js',
+    });
+  }
+});
+
+test('writeTrackingCodeTree returns a tree containing a file that has the tracking code.', function(assert) {
+  var newRelicConfig = EmberNewRelic.newRelicConfig = this.newRelicConfig;
+  var tree = EmberNewRelic.writeTrackingCodeTree();
+  var builder = new broccoli.Builder(tree);
+
+  // Stop the test runner for async tests.
+  stop();
+
+  return builder.build().then(function(results){
+    var contents;
+
+    // Start up the test runner for async tests, because then assert will have the appropriate context.
+    start();
+
+    contents = fs.readFileSync(path.join(results.directory, EmberNewRelic.outputPath), 'utf-8');
+    assert.equal(contents, EmberNewRelic.getNewRelicTrackingCode(newRelicConfig));
+  });
 });
