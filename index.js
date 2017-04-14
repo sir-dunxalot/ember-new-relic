aa8aa74578192608833fc319b18a63a428200b6c
Dan Wenzel <dwenzel@pagerduty.com>
2017-04-14 09:22:46 -0500

    Don't show lack of properties warning if !applicationId
    According to the docs, you can disable New Relic for a certain environment by omitting the applicationId. Therefore, we shouldn't show the lack of properties warning if they omit the applicationId.

--- index.js
+++ index.js
@@ -33,7 +33,9 @@ module.exports = {
     var outputPath = this.outputPath = 'outputPath' in options ? options.outputPath : this.outputPath;
     var isValidNewRelicConfig = this.isValidNewRelicConfig = newRelicConfig.applicationID && newRelicConfig.licenseKey;
 
-    if (!isValidNewRelicConfig) {
+    // Don't show the warning if they didn't provide an applicationID
+    var showLackOfPropertiesWarning = newRelicConfig.applicationID && !isValidNewRelicConfig;
+    if (showLackOfPropertiesWarning) {
       this._writeWarnLine('New Relic config needs `applicationId` and `licenseKey` properties in order to output New Relic script.');
     }
 
