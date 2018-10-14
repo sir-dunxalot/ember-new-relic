042783373dbc69143c364fe9f2a31d3d8ae759a3
Duncan Walker <duncangrahamwalker@gmail.com>
2018-10-13 20:03:35 -0400

    Using Yarn for ember-try

--- config/ember-try.js
+++ config/ember-try.js
@@ -9,7 +9,20 @@ module.exports = function() {
     getChannelURL('canary')
   ]).then((urls) => {
     return {
+      useYarn: true,
       scenarios: [
+        {
+          name: 'ember-lts-2.12',
+          env: {
+            EMBER_OPTIONAL_FEATURES: JSON.stringify({ 'jquery-integration': true }),
+          },
+          npm: {
+            devDependencies: {
+              '@ember/jquery': '^0.5.1',
+              'ember-source': '~2.12.0'
+            }
+          }
+        },
         {
           name: 'ember-lts-2.16',
           env: {
